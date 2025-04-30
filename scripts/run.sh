97e2fb0aa3d05fb9574884cbbd97f13cd0942b52
navazjm <michaelnavs@gmail.com>
2025-04-29 23:18:59 -0500

    chore: update scripts/run.sh to accept new arg to reset imgui.ini by purging it

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,19 +5,43 @@
 
 EXECUTABLE=./build/debug/sortify_d
 
-if [ "$1" == "--build" ]; then
+# Parse args
+DO_BUILD=false
+DO_PURGE_CFG=false
+
+# Iterate over all input arguments
+for arg in "$@"; do
+    case "$arg" in
+        --build)
+            DO_BUILD=true
+            ;;
+        --purge-cfg)
+            DO_PURGE_CFG=true
+            ;;
+        *)
+            # Ignore unknown args from being passed to the executable
+            ;;
+    esac
+done
+
+if [ "$DO_PURGE_CFG" = true ]; then
+    echo "Purging $HOME/.config/sortify"
+    rm -rf "$HOME/.config/sortify"
+fi
+
+if [ "$DO_BUILD" = true ]; then
     if ! ./scripts/build.sh; then
         echo "Debug - Build failed!"
         exit 1
     fi
-
     echo "Debug - Build succeeded!"
-    shift # Remove the --build arg so it doesn't get passed to the executable
 fi
 
+# Run the executable if it exists
 if [ -f "$EXECUTABLE" ]; then
-    "$EXECUTABLE" "$@"
+    "$EXECUTABLE"
 else
     echo "Executable not found at $EXECUTABLE. Try running with --build to compile it first."
     exit 1
 fi
+
